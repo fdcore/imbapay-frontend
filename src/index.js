85521c4072fdbdfdc7f484326f63e0704284c46d
Ilfir <ilfir803@gmail.com>
2024-04-25 14:26:22 +0500

    start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+import './blocks/containers.css'
+// import './blocks/funding_store.css'
+// import './blocks/helpers.css'
+// import './blocks/menu.css'
+// import './blocks/profile.css'
+// import './blocks/footer.css'
+
+// import './components/elements/button.css'
---
+++ tailwind.config.js
@@ -0,0 +1,113 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  darkMode: false,
+  purge: [
+    './html/**/*.{html,js}',
+    './build/tournaments/html/**/*.{html,js}',
+    './dist/**/*.{html,js}',
+    './apps/**/templates/**/*.html',
+    './apps/**/templates/**/**/*.html'
+  ],
+
+  theme: {
+    fontFamily: {
+      AeonikProR: ['AeonikPro-R', 'sans-serif']
+    },
+    spacing: {
+      sm: '0.5rem', // 8px
+      md: '0.75rem', // 12px
+      lg: '1rem', // 16px
+      xl: '1.5rem', // 24px // 6
+      ...defaultTheme.spacing
+    },
+    screens: {
+      // => @media (min-width: 375px) { ... }
+      // 'xs': '375px',
+      ss: {
+        max: '320px'
+      },
+      xs: {
+        min: '320px'
+      },
+      // => @media (min-width: 375px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1320px'
+      // => @media (min-width: 1024px) { ... }
+    },
+    fontSize: {
+      '2xs': [
+        '0.625rem',
+        {
+          lineHeight: '24px'
+        }
+      ], // 10px
+      xs: [
+        '0.75rem',
+        {
+          lineHeight: '16px'
+        }
+      ], // 12px
+      sm: [
+        '0.875rem',
+        {
+          lineHeight: '32px'
+        }
+      ], // 14px
+      base: [
+        '1rem',
+        {
+          lineHeight: '24px'
+        }
+      ], // 16px
+      lg: [
+        '1.125rem',
+        {
+          lineHeight: '1.75rem'
+        }
+      ], // 20px
+      xl: [
+        '1.5rem',
+        {
+          lineHeight: '32px'
+        }
+      ], // 24px
+      '2xl': [
+        '1.7rem',
+        {
+          lineHeight: '28px'
+        }
+      ] // 28px
+    },
+    colors: {
+      current: colors.current,
+      transparent: colors.transparent,
+      stroke20: 'rgba(232, 235, 230, 0.2)',
+      bgdark: 'rgba(34, 34, 34, 0.1)',
+      stroke50: 'rgba(232, 235, 230, 0.5)',
+      black: '#000',
+      grey: '#9b9b9b',
+      main: '#d7ff00',
+      black2: ' #090a08',
+      white: '#e8ebe6',
+      white2: '#fff',
+      white10: 'rgba(255, 255, 255, 0.1)',
+      red: '#a8200d',
+      bg1: '#161617',
+      bg3: '#27272a',
+      bg2: '#18181b',
+      red2: '#ff2102'
+    },
+    fill: (theme) => theme('colors'),
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/typography')]
+}
