41169540e45743159264da343a9db7618c082308
Ilfir <ilfir803@gmail.com>
2024-05-06 13:32:56 +0500

    plan

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require('tailwindcss')
 module.exports = {
-    plugins: {
-<<<<<<< HEAD
-=======
-        "postcss-import": {},
->>>>>>> 6066cf6363c5a5d4a8092983681dce8287f60ae1
-        tailwindcss: {},
-        autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production' ? {
-            cssnano: {}
-        } : {})
-    }
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          cssnano: {}
+        }
+      : {})
+  }
 }
