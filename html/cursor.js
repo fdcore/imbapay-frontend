1cef032ffdcfd8b3919e4627bc518fcb27a0ca37
Ilfir <ilfir803@gmail.com>
2024-05-22 15:54:14 +0500

    upgrade pages

--- html/cursor.js
+++ html/cursor.js
@@ -2,22 +2,15 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
     .test(navigator.userAgent)) {
       // Курсор здесь уже не нужен...
 document.documentElement.style.cursor = 'none';
-document.querySelector('.cursor').style.display='none';
 document.querySelector('.cursor2').style.display='none';
 
 } else {console.log("Вы используете ПК.") ;
   // Курсор здесь уже не нужен...
 }
 console.log("Вы используете мобильное устройство (телефон или планшет).")
-var cursor = document.querySelector('.cursor');
     var cursorinner = document.querySelector('.cursor2');
-    var a = document.querySelectorAll('a');
   
-    document.addEventListener('mousemove', function (e) {
-      var x = e.clientX;
-      var y = e.clientY;
-      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-    });
+    
 
     document.addEventListener('mousemove', function (e) {
       var x = e.clientX;
@@ -27,20 +20,12 @@ var cursor = document.querySelector('.cursor');
     });
 
     document.addEventListener('mousedown', function () {
-      cursor.classList.add('click');
       cursorinner.classList.add('cursorinnerhover')
     });
 
     document.addEventListener('mouseup', function () {
-      cursor.classList.remove('click')
       cursorinner.classList.remove('cursorinnerhover')
     });
+   
 
-    a.forEach(item => {
-      item.addEventListener('mouseover', () => {
-        cursor.classList.add('hover');
-      });
-      item.addEventListener('mouseleave', () => {
-        cursor.classList.remove('hover');
-      });
-    })
+   
